758ec0a48c32eeba3d2fe3fe893095d66d206087
Mateo González <minidrw@gmail.com>
2025-03-29 18:13:10 -0300

    feat: add content data

---
+++ src/content/content.ts
@@ -0,0 +1,204 @@
+export const tldr = {
+    name: 'Mateo González',
+    english: {
+        switch : {
+            theme: {
+                dark: 'dark',
+                light: 'light',
+            },
+            language: {
+                english: 'english',
+                spanish: 'español',
+            }
+        },
+        labor: 'frontend developer',
+        sections: {
+            introduction: {
+                title: 'profile',
+                intro: 'Junior Frontend Developer passionate about creating attractive, responsive, and user-optimized interfaces. Skilled in HTML, CSS, JavaScript, and React, with experience in design. A fast learner, problem-solver, and team player. While my primary focus is on frontend development, I am actively expanding my backend knowledge and eager to enhance my skills in that area.',
+            },
+            skill: {   
+                title: 'skills',
+                skills: [
+                    {
+                        area: 'frontend',
+                        tools: ['HTML', 'CSS', 'JavaScript', 'React.js', 'TailwindCSS']
+                    },
+                    {
+                        area: 'backend',
+                        tools: ['Node.js', 'Express.js'],
+                    },
+                    {
+                        area: 'vcs',
+                        tools: ['Git'],
+                    },
+                    {
+                        area: 'design',
+                        tools: ['Figma'],
+                    },
+                    {
+                        area: 'other',
+                        tools: ['Github'],
+                    },
+                    {
+                        area: 'currently learning',
+                        tools: ['Astro', 'TypeScript']
+                    }
+                ]
+            },
+        
+            education: {
+                title: 'education',
+                carrers: [
+                    {
+                        name: 'University Degree in Programming',
+                        university: 'Universidad Tecnologica Nacional',
+                        startDate: 'april 2022',
+                        finishDate: 'december 2024',
+                        duration: {
+                            number: 2,
+                            years: 'years',
+                        },
+                        location: "Argentina, Tucumán",
+                    },
+                ],
+                courses: [
+                    {
+                        name: 'english institute',
+                        university: 'ATICANA',
+                        startDate: null,
+                        finishDate: null,
+                        duration: {
+                            number: 8,
+                            years: 'years'
+                        },
+                        location: "Argentina, Tucumán",
+                    },
+                ]
+            },
+        
+            language: {
+                title: 'languages',
+                languages: [
+                    {
+                        name: 'spanish',
+                        level: 'native',
+                        cefr: null,
+                    },
+                    {
+                        name: 'english',
+                        level: 'intermediate',
+                        cefr: 'B2',
+                    },
+                ] 
+            },
+        
+            cv: {
+                text: 'My CV',
+                fileName: '/MateoGonzalez_CV_English.pdf',
+            },
+        }
+    },
+    spanish: {
+        spanish: {
+            switch : {
+                theme: {
+                    dark: 'oscuro',
+                    light: 'claro',
+                },
+                language: {
+                    english: 'ingles',
+                    spanish: 'español',
+                }
+            },
+            labor: 'desarrollador frontend',
+            sections: {
+                introduction: {
+                    title: 'perfil',
+                    intro: 'Desarrollador Frontend Junior apasionado por crear interfaces atractivas, responsivas y optimizadas para el usuario. Con conocimientos en HTML, CSS, JavaScript y React, así como experiencia en diseño. Aprendiz rápido, resolutivo y con habilidades para el trabajo en equipo. Si bien me encuentro mas enfocado en front, estoy aprendiendo mas de backend y me encuentro dispuesto a mejorar en ese área.'
+                },
+                skill: {
+                    title: 'habilidades',
+                    skills: [
+                        {
+                            area: 'frontend',
+                            tools: ['HTML', 'CSS', 'JavaScript', 'React.js', 'TailwindCSS']
+                        },
+        
+                        {
+                            area: 'backend',
+                            tools: ['Node.js', 'Express.js'],
+                        },
+        
+                        {
+                            area: 'vcs',
+                            tools: ['Git']
+                        },
+        
+                        {
+                            area: 'design',
+                            tools: ['Figma']
+                        },
+        
+                        {
+                            area: 'otras',
+                            tools: ['Github']
+                        },
+                        {
+                            area: 'actualmente aprendiendo',
+                            tools: ['Astro', 'TypeScript']
+                        }
+                    ],
+                },
+                education: {
+                    title: 'educacion',
+                    carrers: [
+                        {
+                            name: 'Tecnicatura Universitaria en Programación',
+                            university: 'Universidad Tecnologica Nacional',
+                            startDate: 'Abril 2022',
+                            finishDate: 'Diciembre 2024',
+                            duration: {
+                                number: 2,
+                                years: 'años'
+                            },
+                            location: "Argentina, Tucumán",
+                        },
+                    ],
+                    courses: [
+                        {
+                            name: 'Instituto de Inglés',
+                            university: 'ATICANA',
+                            startDate: null,
+                            finishDate: null,
+                            duration: {
+                                number: 8,
+                                years: 'años'
+                            },
+                            location: "Argentina, Tucumán",
+                        },
+                    ]
+                },
+                language: {
+                    title: 'lenguajes',
+                    languages: [
+                        {
+                            name: 'español',
+                            level: 'native',
+                            cefr: null,
+                        },
+                        {
+                            name: 'ingles',
+                            level: 'intermediate',
+                            cefr: 'b2'
+                        }
+                    ]
+                },
+                cv: {
+                    text: 'Mi CV',
+                    fileName: '/MateoGonzalez_CV_Spanish.pdf',
+                }
+            }
+        }
+    },
+}
